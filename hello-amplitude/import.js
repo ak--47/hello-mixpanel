d3a2fe1af53d70d7f7c0b16666e084c98fd74f1f
AK <akrivit@gmail.com>
2025-05-25 12:10:50 -0700

    fix amp

--- hello-amplitude/import.js
+++ hello-amplitude/import.js
@@ -13,6 +13,8 @@ const AMP_EVENTS_ENDPOINT = `https://api.amplitude.com/2/httpapi`;
 const AMP_USERS_ENDPOINT = `https://api2.amplitude.com/identify`;
 const AMP_API_KEY = process.env.AMPLITUDE_API_KEY;
 
+if (!AMP_API_KEY) throw new Error("Missing AMPLITUDE_API_KEY in .env file. Please add it to your .env file.");
+
 // ? https://www.docs.developers.amplitude.com/analytics/apis/http-v2-api/#upload-request
 
 /**
@@ -22,39 +24,6 @@ async function main(spec = dataSpec) {
 	console.log("\nGenerating Amplitude data...\n\n");
 	const data = await generate(spec);
 	const { eventData, userProfilesData } = data;
-
-	const amplitudeEvents = eventData.map(event => {
-		const ampEvent = {
-			user_id: event.$user_id,
-			event_type: event.event,
-			...event,
-		};
-		delete ampEvent.$user_id;
-		delete ampEvent.event;
-		delete ampEvent.$source;
-		ampEvent.time = dayjs(event.time).unix();
-		return ampEvent;
-
-		
-	});
-
-	/** @type {BatchReqConfig} */
-	const eventsJobConfig = {
-		url: AMP_EVENTS_ENDPOINT,
-		data: amplitudeEvents,
-		batchSize: 100,
-		concurrency: 10,
-		bodyParams: {
-			api_key: AMP_API_KEY,
-			dataKey: "events",
-		},
-	};
-
-	console.log(`sending ${u.comma(amplitudeEvents.length)} events to Amplitude...\n`);
-	const eventResponses = await batchQueue(eventsJobConfig);
-	const eventsSent = eventResponses.reduce((acc, res) => acc + res.events_ingested, 0);
-	console.log(`\n... sent ${u.comma(eventsSent)} events to Amplitude.`);
-
 
 	const amplitudeUsers = userProfilesData.map(user => {
 		const ampUser = {
@@ -82,6 +51,41 @@ async function main(spec = dataSpec) {
 	console.log(`\nsending ${u.comma(amplitudeUsers.length)} users to Amplitude...\n`);
 	const userResponses = await batchQueue(usersJobConfig);
 	console.log(`\n... sent ${u.comma(amplitudeUsers.length)} users to Amplitude.`);
+
+	const amplitudeEvents = eventData.map(event => {
+		const ampEvent = {
+			user_id: event.$user_id,
+			event_type: event.event,
+			event_properties: { ...event }
+		};
+		delete ampEvent.$user_id;
+		delete ampEvent.event;
+		delete ampEvent.$source;
+		ampEvent.time = dayjs(event.time).unix();
+		return ampEvent;
+
+
+	});
+
+	/** @type {BatchReqConfig} */
+	const eventsJobConfig = {
+		url: AMP_EVENTS_ENDPOINT,
+		data: amplitudeEvents,
+		batchSize: 100,
+		concurrency: 10,
+		bodyParams: {
+			api_key: AMP_API_KEY,
+			dataKey: "events",
+		},
+	};
+
+	console.log(`sending ${u.comma(amplitudeEvents.length)} events to Amplitude...\n`);
+	const eventResponses = await batchQueue(eventsJobConfig);
+	const eventsSent = eventResponses.reduce((acc, res) => acc + res.events_ingested, 0);
+	console.log(`\n... sent ${u.comma(eventsSent)} events to Amplitude.`);
+
+
+
 
 	return {
 		users: userResponses,
